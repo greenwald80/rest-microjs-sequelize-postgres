b4899f73425d20cb0b31ff72e0345bd13fa670b8
Alexander <greenwald80@gmail.com>
2021-02-27 22:55:07 +0200

    first commit

---
+++ src/db.js
@@ -0,0 +1,27 @@
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+const sequelize = new Sequelize('micro_intro','postgres', '1234', {
+    dialect:'postgres'
+})
+
+class Record extends Model {}
+
+Record.init(
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        text: DataTypes.STRING
+    },
+    {
+        sequelize,
+        modelName: 'record'
+    }
+)
+
+sequelize.sync()
+
+module.exports = {
+    Record,
+}
+
+
+module.exports = sequelize;
